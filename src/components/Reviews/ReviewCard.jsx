faae56a63bd9413bfdf09e893bf81dd2bd914e52
hynesnd <hynesnd@gmail.com>
2021-11-23 11:29:36 +0000

    adds basic ReviewCard component

--- src/components/Reviews/ReviewCard.jsx
+++ src/components/Reviews/ReviewCard.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
-const ReviewCard = () => {
-  return <div>ReviewCard</div>;
+const ReviewCard = ({ review }) => {
+  return (
+    <div className="review-card">
+      <h3>{review.title}</h3>
+      <h3>{review.category}</h3>
+      <img src={review.review_img_url} alt={review.title} />
+      <p>{review.owner}</p>
+      <p>{review.comment_count}</p>
+      <button>Vote</button>
+    </div>
+  );
 };
 
 export default ReviewCard;
