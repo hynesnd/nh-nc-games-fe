056910597ffbcdf09613edcee15c043fe329f6f8
hynesnd <hynesnd@gmail.com>
2021-11-23 11:19:41 +0000

    adds useEffect getReviews, to add category query and ReviewCard

--- src/components/Reviews/ReviewsList.jsx
+++ src/components/Reviews/ReviewsList.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import { getReviews } from "../../utils/api";
 
-const ReviewsList = () => {
-  return <div>ReviewsList</div>;
+const ReviewsList = ({ currentCategory }) => {
+  const [reviews, setReviews] = useState([]);
+
+  useEffect(() => {
+    getReviews().then((reviewsFromServer) => {
+      setReviews(reviewsFromServer);
+    });
+  }, []);
+
+  return (
+    <ul>
+      {reviews.map((review) => {
+        return <li>{review.title}</li>;
+      })}
+    </ul>
+  );
 };
 
 export default ReviewsList;
