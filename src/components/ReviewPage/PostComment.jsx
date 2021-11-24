b3d71052df789710fb4ca39d1135e3eccb38e631
hynesnd <hynesnd@gmail.com>
2021-11-24 16:29:08 +0000

    Completes basic PostComment component

--- src/components/ReviewPage/PostComment.jsx
+++ src/components/ReviewPage/PostComment.jsx
@@ -1,7 +1,39 @@
-import React from "react";
+import { useState } from "react";
+import { useContext } from "react/cjs/react.development";
+import { UserContext } from "../../contexts/UserContext";
+import { postComment } from "../../utils/api";
 
-const PostComment = () => {
-  return <div>PostComment</div>;
+const PostComment = ({ review_id, setComments }) => {
+  const [commentInput, setCommentInput] = useState();
+  const { user, isLoggedIn } = useContext(UserContext);
+
+  return (
+    <div>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          postComment(review_id, user.username, commentInput).then(
+            (newComment) => {
+              setComments((prevState) => {
+                const newState = [newComment, ...prevState];
+                return newState;
+              });
+            }
+          );
+        }}
+      >
+        <textarea
+          placeholder="comment"
+          rows="4"
+          required
+          value={commentInput}
+          onChange={(event) => setCommentInput(event.target.value)}
+        ></textarea>{" "}
+        <br />
+        <input type="submit" value="Post comment" />
+      </form>
+    </div>
+  );
 };
 
 export default PostComment;
