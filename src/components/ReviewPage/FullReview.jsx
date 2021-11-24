f632e119ddc48ab6629f15ec934548b217a26dea
hynesnd <hynesnd@gmail.com>
2021-11-24 12:44:41 +0000

    completes basic  FullReview component

--- src/components/ReviewPage/FullReview.jsx
+++ src/components/ReviewPage/FullReview.jsx
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+import { useEffect } from "react/cjs/react.development";
+import { getSingleReview } from "../../utils/api";
 
-const FullReview = () => {
-  return <div>FullReview</div>;
+const FullReview = ({ review_id }) => {
+  const [review, setReview] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setIsLoading(true);
+    getSingleReview(review_id).then((review) => {
+      setReview(review);
+      setIsLoading(false);
+    });
+  }, []);
+
+  if (isLoading) {
+    return <span>Loading...</span>;
+  } else {
+    return (
+      <div className="full-review">
+        <h2>{review.title}</h2>
+        <h3>{review.category}</h3>
+        <img src={review.review_img_url} alt={review.title} />
+        <p>Review author: {review.owner}</p>
+        <p>Designer: {review.designer}</p>
+        <p>{review.review_body}</p>
+      </div>
+    );
+  }
 };
 
 export default FullReview;
