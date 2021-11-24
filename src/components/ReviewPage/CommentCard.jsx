46557f4182f6f70ee7491a836d0da042148ba7ea
hynesnd <hynesnd@gmail.com>
2021-11-24 14:32:38 +0000

    completes basic CommentCard component

--- src/components/ReviewPage/CommentCard.jsx
+++ src/components/ReviewPage/CommentCard.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
-const CommentCard = () => {
-  return <div>CommentCard</div>;
+const CommentCard = ({ comment }) => {
+  return (
+    <div className="comment-card">
+      <p>Author: {comment.author}</p>
+      <p>Date: {comment.created_at}</p>
+      <p>{comment.body}</p>
+      <button>Vote</button>
+    </div>
+  );
 };
 
 export default CommentCard;
