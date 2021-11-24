a31fbc1fd27b90a784b8f4fd02e6d65a6aa661b5
hynesnd <hynesnd@gmail.com>
2021-11-24 16:42:32 +0000

    adds handleDelete functionality to delete comment button and isdeleted flag to replace delete comments

--- src/components/ReviewPage/CommentCard.jsx
+++ src/components/ReviewPage/CommentCard.jsx
@@ -1,21 +1,35 @@
 import React from "react";
-import { useContext } from "react/cjs/react.development";
+import { useContext, useState } from "react";
 import { UserContext } from "../../contexts/UserContext";
+import { deleteComment } from "../../utils/api";
 
 const CommentCard = ({ comment }) => {
   const { user } = useContext(UserContext);
+  const [isDeleted, setDeleted] = useState(false);
 
-  return (
-    <div className="comment-card">
-      <p>Author: {comment.author}</p>
-      <p>Date: {comment.created_at}</p>
-      <p>{comment.body}</p>
-      {comment.author === user.username ? (
-        <button>Delete comment</button>
-      ) : null}
-      <button>Vote</button>
-    </div>
-  );
+  const handleDelete = (comment_id) => {
+    deleteComment(comment_id).then((res) => {
+      setDeleted(true);
+    });
+  };
+
+  if (isDeleted) {
+    return <span>Comment deleted.</span>;
+  } else {
+    return (
+      <div className="comment-card">
+        <p>Author: {comment.author}</p>
+        <p>Date: {comment.created_at}</p>
+        <p>{comment.body}</p>
+        {comment.author === user.username ? (
+          <button onClick={() => handleDelete(comment.comment_id)}>
+            Delete comment
+          </button>
+        ) : null}
+        <button>Vote</button>
+      </div>
+    );
+  }
 };
 
 export default CommentCard;
