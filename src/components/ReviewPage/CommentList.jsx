7f38363b93587b5968431422504de939f23b8e55
hynesnd <hynesnd@gmail.com>
2021-11-24 14:33:21 +0000

    completes basic comment list with map of commentCard

--- src/components/ReviewPage/CommentList.jsx
+++ src/components/ReviewPage/CommentList.jsx
@@ -1,7 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
+import { useEffect } from "react/cjs/react.development";
+import { getComments } from "../../utils/api";
+import CommentCard from "./CommentCard";
 
-const CommentList = () => {
-  return <div>CommentList</div>;
+const CommentList = ({ review_id }) => {
+  const [comments, setComments] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [errSpan, setErrSpan] = useState(null);
+
+  useEffect(() => {
+    setIsLoading(true);
+    setErrSpan(null);
+    getComments(review_id)
+      .then((commentsFromApi) => {
+        setComments(commentsFromApi);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        if (err.response.status === 404) {
+          setIsLoading(false);
+          setErrSpan("No comments yet!");
+        }
+      });
+  }, []);
+
+  console.log(comments);
+
+  if (isLoading) {
+    return <span>Loading...</span>;
+  } else if (errSpan) {
+    return <span>{errSpan}</span>;
+  } else {
+    return (
+      <div>
+        {comments.map((comment) => {
+          return <CommentCard key={comment.comment_id} comment={comment} />;
+        })}
+      </div>
+    );
+  }
 };
 
 export default CommentList;
