526c8554761d09a7cc8b7b8399bf3ed884893360
hynesnd <hynesnd@gmail.com>
2021-11-24 16:30:56 +0000

    moves PostComment component inside CommentList for easier state access

--- src/components/ReviewPage/ReviewPage.jsx
+++ src/components/ReviewPage/ReviewPage.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import CommentList from "./CommentList";
 import FullReview from "./FullReview";
-import PostComment from "./PostComment";
 
 const ReviewPage = () => {
   const { review_id } = useParams();
@@ -10,7 +9,6 @@ const ReviewPage = () => {
   return (
     <div>
       <FullReview review_id={review_id} />
-      <PostComment review_id={review_id} />
       <CommentList review_id={review_id} />
     </div>
   );
