6a0653356e4dd8086288e34c1eb186cdd23c711a
hynesnd <hynesnd@gmail.com>
2021-11-24 16:25:36 +0000

    adds placeholder delete comment button

--- src/components/ReviewPage/CommentCard.jsx
+++ src/components/ReviewPage/CommentCard.jsx
@@ -1,11 +1,18 @@
 import React from "react";
+import { useContext } from "react/cjs/react.development";
+import { UserContext } from "../../contexts/UserContext";
 
 const CommentCard = ({ comment }) => {
+  const { user } = useContext(UserContext);
+
   return (
     <div className="comment-card">
       <p>Author: {comment.author}</p>
       <p>Date: {comment.created_at}</p>
       <p>{comment.body}</p>
+      {comment.author === user.username ? (
+        <button>Delete comment</button>
+      ) : null}
       <button>Vote</button>
     </div>
   );
