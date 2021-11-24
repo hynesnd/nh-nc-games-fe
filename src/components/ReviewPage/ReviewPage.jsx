f0149d50b8de1810bfd4df66ab0e4e6fe4fc9f71
hynesnd <hynesnd@gmail.com>
2021-11-24 12:45:30 +0000

    adds useParam to capture review_id fromurl and adds as prop to child components

--- src/components/ReviewPage/ReviewPage.jsx
+++ src/components/ReviewPage/ReviewPage.jsx
@@ -1,14 +1,17 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 import CommentList from "./CommentList";
 import FullReview from "./FullReview";
 import PostComment from "./PostComment";
 
 const ReviewPage = () => {
+  const { review_id } = useParams();
+
   return (
     <div>
-      <FullReview />
-      <PostComment />
-      <CommentList />
+      <FullReview review_id={review_id} />
+      <PostComment review_id={review_id} />
+      <CommentList review_id={review_id} />
     </div>
   );
 };
