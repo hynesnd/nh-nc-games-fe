3a5d29b442937bc148315efa27d7fe835e0c15ad
hynesnd <hynesnd@gmail.com>
2021-11-24 12:09:34 +0000

    Adds link to ReviewPage on review title

--- src/components/Reviews/ReviewCard.jsx
+++ src/components/Reviews/ReviewCard.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const ReviewCard = ({ review }) => {
   return (
     <div className="review-card">
-      <h3>{review.title}</h3>
+      <Link to={`/reviews/${review.review_id}`}>
+        <h3>{review.title}</h3>
+      </Link>
       <h3>{review.category}</h3>
       <img src={review.review_img_url} alt={review.title} />
       <p>{review.owner}</p>
