84c52a52c9815b05b3af908251d68ab6743e3833
hynesnd <hynesnd@gmail.com>
2021-11-25 14:22:23 +0000

    re-factors NavBar to have get categories and map links to separate category pages to display reviews

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,26 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import { getCategories } from "../utils/api";
 
 const NavBar = () => {
+  const [categoryList, setCategoryList] = useState([]);
+
+  useEffect(() => {
+    getCategories().then((categoriesFromApi) => {
+      setCategoryList(categoriesFromApi);
+    });
+  }, []);
+
   return (
     <nav>
-      <Link to="/">Reviews</Link>
+      <Link to="/">All Reviews</Link>
+      {categoryList.map((category) => {
+        return (
+          <Link key={category.slug} to={`/categories/${category.slug}`}>
+            {category.slug}
+          </Link>
+        );
+      })}
       <Link to="/login">Login</Link>
     </nav>
   );
