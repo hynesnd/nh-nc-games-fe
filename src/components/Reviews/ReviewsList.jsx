b748f931e9de803342ca028f8ce4184be06d5697
hynesnd <hynesnd@gmail.com>
2021-11-25 14:49:06 +0000

    adds 404 error catch to getReviews call

--- src/components/Reviews/ReviewsList.jsx
+++ src/components/Reviews/ReviewsList.jsx
@@ -5,19 +5,28 @@ import ReviewCard from "./ReviewCard";
 const ReviewsList = ({ currentCategory, queries }) => {
   const [reviews, setReviews] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [errSpan, setErrSpan] = useState(null);
 
   useEffect(() => {
+    setErrSpan(false);
     setIsLoading(true);
-    getReviews(currentCategory, queries.sort_by, queries.order).then(
-      (reviewsFromServer) => {
+    getReviews(currentCategory, queries.sort_by, queries.order)
+      .then((reviewsFromServer) => {
         setReviews(reviewsFromServer);
         setIsLoading(false);
-      }
-    );
+      })
+      .catch((err) => {
+        if (err.response.status === 404) {
+          setIsLoading(false);
+          setErrSpan("Category not found.");
+        }
+      });
   }, [currentCategory, queries]);
 
   if (isLoading) {
     return <span>Loading...</span>;
+  } else if (errSpan) {
+    return <span>{errSpan}</span>;
   } else {
     return (
       <ul>
