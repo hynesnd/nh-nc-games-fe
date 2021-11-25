68f5440f28fb15ce792ad2de10254ee40a46ab6d
hynesnd <hynesnd@gmail.com>
2021-11-25 11:32:34 +0000

    adds comment voting and optimistic rendering behaviour to VoteButtons

--- src/components/VoteButtons.jsx
+++ src/components/VoteButtons.jsx
@@ -1,7 +1,32 @@
-import React from "react";
+import { useState } from "react";
+import { patchCommentVotes } from "../utils/api";
 
-const VoteButtons = () => {
-  return <div></div>;
+const VoteButtons = ({ voteType, voteId, votes }) => {
+  const [voteCount, setVoteVount] = useState(votes);
+  const [addedVotes, setAddedVotes] = useState(0);
+
+  const handleUpvote = () => {
+    if (voteType === "comment") {
+      setAddedVotes((prevVotes) => prevVotes + 1);
+      patchCommentVotes(voteId, 1);
+    }
+  };
+
+  const handleDownvote = () => {
+    if (voteType === "comment") {
+      console.log("in downvote");
+      setAddedVotes((prevVotes) => prevVotes - 1);
+      patchCommentVotes(voteId, -1);
+    }
+  };
+
+  return (
+    <div>
+      <button onClick={handleUpvote}>⬆️</button>
+      <p>{voteCount + addedVotes}</p>
+      <button onClick={handleDownvote}>⬇️</button>
+    </div>
+  );
 };
 
 export default VoteButtons;
