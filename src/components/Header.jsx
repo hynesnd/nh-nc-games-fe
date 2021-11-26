53b9f335340d9103267e972880b141bf7e44703b
hynesnd <hynesnd@gmail.com>
2021-11-26 10:32:34 +0000

    adds styled components to Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,17 @@
+import styled from "styled-components";
+
+const StyledHeader = styled.header`
+  background: #0a1931;
+  color: #ffc947;
+
+  padding: 0.25em 1em;
+`;
+
 const Header = ({ text, styling }) => {
   return (
-    <header className={styling}>
+    <StyledHeader className={styling}>
       <h1>{text}</h1>
-    </header>
+    </StyledHeader>
   );
 };
 
