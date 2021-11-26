b528ad242af52280e68e20c356aded095b7d9318
hynesnd <hynesnd@gmail.com>
2021-11-26 15:12:25 +0000

    adds styled commponents to Login

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,7 +1,22 @@
 import { useContext, useState } from "react";
 import { UserContext } from "../../contexts/UserContext";
 import { getUser } from "../../utils/api";
+import styled from "styled-components";
 
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 300px;
+  border-radius: 18px;
+  border: solid 2px black;
+  text-align: center;
+  align-items: center;
+  background: #feddbe;
+  padding: 2px;
+  margin: auto;
+`;
+
+const form = styled.form``;
 const Login = () => {
   const { user, setUser, isLoggedIn, logout } = useContext(UserContext);
   const [formInput, setFormInput] = useState("");
@@ -9,7 +24,7 @@ const Login = () => {
 
   if (!isLoggedIn) {
     return (
-      <div className="login-form">
+      <Container className="login-form">
         <form
           onSubmit={(event) => {
             event.preventDefault();
@@ -37,14 +52,14 @@ const Login = () => {
           </fieldset>
         </form>
         <span>{errorSpan}</span>
-      </div>
+      </Container>
     );
   } else {
     return (
-      <div>
+      <Container>
         <h2>Welcome {user.username}</h2>
         <button onClick={() => logout()}>Logout</button>
-      </div>
+      </Container>
     );
   }
 };
