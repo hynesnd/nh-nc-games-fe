c5234b9263bbd79df6204211b62e60feb05d6916
hynesnd <hynesnd@gmail.com>
2021-11-26 11:39:34 +0000

    adds styled components to VoteButtons

--- src/components/VoteButtons.jsx
+++ src/components/VoteButtons.jsx
@@ -1,6 +1,31 @@
 import { useState, useContext } from "react";
 import { patchCommentVotes, patchReviewVotes } from "../utils/api";
 import { UserContext } from "../contexts/UserContext";
+import styled from "styled-components";
+
+const Button = styled.button`
+  background: transparent;
+  border-radius: 3px;
+  border: 2px solid #0a1931;
+  color: #0a1931;
+  margin: 0 1em;
+  padding: 0.25em 0.25em;
+`;
+
+const StyledP = styled.p`
+  padding: 2px;
+  color: #0a1931;
+  font-weight: bold;
+  border: 2px solid #0a1931;
+  border-radius: 3px;
+`;
+
+const Container = styled.div`
+  background: transparent;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 const VoteButtons = ({ voteType, voteId, votes }) => {
   const { isLoggedIn } = useContext(UserContext);
@@ -29,15 +54,15 @@ const VoteButtons = ({ voteType, voteId, votes }) => {
   };
 
   return (
-    <div className="vote-buttons">
-      <button onClick={handleUpvote} disabled={!isLoggedIn}>
+    <Container className="vote-buttons">
+      <Button onClick={handleUpvote} disabled={!isLoggedIn}>
         ⬆️
-      </button>
-      <p>{votes + addedVotes}</p>
-      <button onClick={handleDownvote} disabled={!isLoggedIn}>
+      </Button>
+      <StyledP>{votes + addedVotes}</StyledP>
+      <Button onClick={handleDownvote} disabled={!isLoggedIn}>
         ⬇️
-      </button>
-    </div>
+      </Button>
+    </Container>
   );
 };
 
