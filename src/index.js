90487de2c8e638555c40dcca4b32c9ba11752f23
hynesnd <hynesnd@gmail.com>
2021-11-29 09:17:11 +0000

    removes react.strictmode

--- src/index.js
+++ src/index.js
@@ -5,10 +5,8 @@ import App from "./App";
 import { UserProvider } from "./contexts/UserContext";
 
 ReactDOM.render(
-  <React.StrictMode>
     <UserProvider>
       <App />
     </UserProvider>
-  </React.StrictMode>,
   document.getElementById("root")
 );
