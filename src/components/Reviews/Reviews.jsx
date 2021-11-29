25b01e9dd5d63bab3b5288d6e9302c49e083e85d
hynesnd <hynesnd@gmail.com>
2021-11-29 13:40:52 +0000

    amends import for useParams to be from react-router-dom

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useParams } from "react-router";
+import { useParams } from "react-router-dom";
 import QuerySelector from "./QuerySelector";
 import ReviewsList from "./ReviewsList";
 
